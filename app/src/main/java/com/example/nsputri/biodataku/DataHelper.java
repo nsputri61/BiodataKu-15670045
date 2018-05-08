dbfd7f27217f74985ddf915e4c9248e6b59cc76a
nadzilaf <nadzila.fahrina@gmail.com>
2018-05-08 19:38:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nsputri/biodataku/DataHelper.java
@@ -0,0 +1,35 @@
+package com.example.nsputri.biodataku;
+
+/**
+ * Created by 10 on 07/05/2018.
+ */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "biodatadiri.db";
+    private static final int DATABASE_VERSION = 1;
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        String sql = "create table biodata(no integer primary key, nama text null, tgl text null, jk text null, alamat text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+        sql = "INSERT INTO biodata (no, nama, tgl, jk, alamat) VALUES ('15670045', 'Nur Septiana Putri', '1997-09-16', 'Perempuan','Pekalongan');";
+        db.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        // TODO Auto-generated method stub
+
+    }
+}
